c3b3a4ad0927d4bb944f1924f424806f1a3d3d0b
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2023-07-19 13:09:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ setInterval(function showtime(){
     hours = hours > 12 ? hours % 12 : hours;
     hours = hours < 10 ? "0" + hours : hours;
     x.innerHTML = hours + ""+ ":";
-     e.innerHTML = hours < 12 ? "AM" :"PM"; 
+    //  e.innerHTML = hours < 12 ? "AM" :"PM"; 
 
     let m=a.getMinutes();
     m=m<10?"0"+m:m;
@@ -31,33 +31,21 @@ function  search(){
 }
 
 
+// search_handler
+// $(document).ready(function(){
+//   $("#search").click(function(){
+//     $("#searchBar").on("keyup", function() {
+//       var value = $(this).val().toLowerCase();
+//       $(".card-body h5").filter(function() {
+//         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//       });
+//     });
+//   })
+// });
 
 
-// setInterval(() => {
-//   //getting id.....
-//   const hEl = document.getElementById('hours');
-//   const mEl = document.getElementById('minutes');
-//   const sEl = document.getElementById('seconds');
-//   // const ampmEl = document.getElementById('ampm');
-//   // const noonEl = document.getElementById('noon');
-//   // ampmEl.innerHTML = h < 12 ? "AM" :"PM";  
-//   // noonEl.innerHTML = h < 12 ? h > 1 ? h > 6?  "MORNING" : "AFTERNOON" :"EVENING" : "NIGHT";
-
-//   //declare variable
-//   var time = new Date();
-//   var h = time.getHours();
-//   var m = time.getMinutes();
-//   var s = time.getSeconds();
-  
-//   //changing the hours 24 to 12 hours
-//   h = h > 12 ? h % 12 :"0"+ h;
-//   h = h < 10 ? "0" + h : h;
-//   m = m < 10 ? "0" + m : m;
-//   s = s < 10 ?  s: s;
-//   //what element we want to changing dynamic....
-//   hEl.innerHTML = h;
-//   mEl.innerHTML = m;
-//   sEl.innerHTML = s;   
-// }, 1000);
-
-
+const btn=document.getElementById("search")
+ btn.addEventListener("click",function(){
+  const x=document.getElementById("searchBar").value
+  console.log(x)
+ })
